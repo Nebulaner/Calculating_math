a51044f3bd0744977350d9040a08deee193bc7bd
Nebulaner <166131396+Nebulaner@users.noreply.github.com>
2026-05-06 17:12:50 +0300

    Update Lab5.py

--- 5 лаба/Lab5.py
+++ 5 лаба/Lab5.py
@@ -1,8 +1,6 @@
 import numpy as np
 
 def gauss(A, b):
-    A = np.array(A, dtype=float)
-    b = np.array(b, dtype=float)
     n = A.shape[0]
     
     Ab = np.hstack([A, b.reshape(-1, 1)])
@@ -36,18 +34,46 @@ def gauss(A, b):
     else:
         print("Система несовместна.")
         return None
+
+def jacobi(A, b, x0=None, tol=1e-6, max_iter=1000):
+    A = np.array(A, dtype=float)
+    b = np.array(b, dtype=float)
+    n = len(b)
     
-A = [
-    [2, 1, -1, 3],
-    [4, 2, -1, 7],
-    [1, 1, 1, 1],
-    [3, 1, -2, 5]
-    ]
+    if x0 is None:
+        x = np.zeros(n)
+    else:
+        x = x0.copy()
     
-b = [1, 3, 2, 0]
+    # Диагональ и её обратные значения
+    diag = np.diag(A)
+    inv_diag = 1 / diag
+    
+    # Матрица без диагонали
+    A_off = A - np.diag(diag)
+    
+    for k in range(max_iter):
+        x_new = inv_diag * (b - np.dot(A_off, x))
+        
+        if np.linalg.norm(x_new - x, np.inf) < tol:
+            return x_new
+        
+        x = x_new
+    
+    return x
+
+A1 = np.array([[4, 1, 0, 1], [1, 5, -1, 0], [0, -1, 4, 1], [1, 0, 1, 5]])
+b1 = np.array([6, 5, 4, 7])
+
+A = np.array(A1)
+b = np.array(b1)
     
 solution = gauss(A, b)
     
 if solution is not None:
     print("Решение x:", solution)
     print("Проверка Ax - b:", np.dot(A, solution) - b)
+    
+x = jacobi(A, b)
+print("Решение:", x)
+print("Невязка:", np.linalg.norm(np.dot(A, x) - b))
